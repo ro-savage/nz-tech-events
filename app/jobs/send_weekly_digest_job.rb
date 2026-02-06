b4bf0f638ef99d56bb4ea77e30a890e2fc4c602b
Rowan Savage <rowan.savage@gmail.com>
2026-02-07 01:21:41 +0530

    Add all events to email

--- app/jobs/send_weekly_digest_job.rb
+++ app/jobs/send_weekly_digest_job.rb
@@ -22,13 +22,23 @@ class SendWeeklyDigestJob < ApplicationJob
                              .order(:start_date)
                              .distinct
 
+
+      # Fetch all new events across New Zealand once per digest run (excluding current region)
+      new_events_nz = Event.upcoming
+                          .approved
+                          .where("events.created_at >= ?", 7.days.ago)
+                          .where.not(region: region)
+                          .order(:start_date)
+                          .distinct
+      
       # Convert to arrays for serialization (deliver_later can't serialize ActiveRecord::Relation)
       new_events_array = new_events.to_a
       upcoming_events_array = upcoming_events.to_a
+      new_events_nz_array = new_events_nz.to_a
 
       # Send email to each subscriber
       subscriptions.find_each do |subscription|
-        WeeklyDigestMailer.digest(subscription, new_events_array, upcoming_events_array).deliver_later
+        WeeklyDigestMailer.digest(subscription, new_events_array, upcoming_events_array, new_events_nz_array).deliver_later
         subscription.mark_sent!
       end
     end
--- app/mailers/weekly_digest_mailer.rb
+++ app/mailers/weekly_digest_mailer.rb
@@ -1,8 +1,9 @@
 class WeeklyDigestMailer < ApplicationMailer
-  def digest(subscription, new_events, upcoming_events)
+  def digest(subscription, new_events, upcoming_events, new_events_nz)
     @subscription = subscription
     @new_events = new_events
     @upcoming_events = upcoming_events
+    @new_events_nz = new_events_nz
     @region_display = subscription.region_display
     @week_of = Date.current.strftime("%B %d, %Y")
     @unsubscribe_url = unsubscribe_url(token: subscription.unsubscribe_token)
